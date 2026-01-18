165b5c76eb3dcceff8842902abee96d3f7b337af
Arthur Zhou <arthuracct@gmail.com>
2026-01-17 21:33:40 -0800

    gautamlover123

--- ihatefrontend3/src/App.js
+++ ihatefrontend3/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 
-// --- TODO: IMPLEMENT THIS COMPONENT ---
 function StockSearch({ stocks }) {
   // TODO 1: Create state for the 'searchTerm'
+  const [searchTerm, setSearchTerm] = useState("");
 
   // TODO 2: Create a logical variable 'filteredStocks'
   // Hint: Use .filter() on the 'stocks' array.
   // Hint: Convert both the stock symbol and the search term to .toLowerCase() so casing doesn't matter.
+  const filteredStocks = stocks.filter((stocks) => {
+    return stocks.symbol.toLowerCase().includes(searchTerm.toLowerCase());
+  })
 
   return (
     <div style={{ padding: "20px", fontFamily: "sans-serif", maxWidth: "400px" }}>
@@ -14,18 +17,29 @@ function StockSearch({ stocks }) {
 
       {/* TODO 3: The Search Input */}
       <input 
-        type="text" 
+        type="text"
         placeholder="Search Ticker..."
         style={{ padding: "8px", width: "100%", marginBottom: "15px", boxSizing: "border-box" }}
-        // Connect this input to your state
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
 
       {/* TODO 4: The List of Results */}
       <div style={{ border: "1px solid #ccc", borderRadius: "5px" }}>
         
         {/* If filteredStocks is empty, show "No results found" */}
-        
         {/* Else, map through filteredStocks and display them */}
+        <div>
+          {filteredStocks.length === 0? <p>No results found</p> : (
+            filteredStocks.map((fS) => (
+              <div key={fS.name} style={{ display: "flex", borderBottom: "1px solid #eee", padding: "8px 0" }}>
+                <strong>
+                  {fS.symbol}
+                </strong>
+              </div>
+            ))
+          )}
+        </div>
 
       </div>
     </div>
